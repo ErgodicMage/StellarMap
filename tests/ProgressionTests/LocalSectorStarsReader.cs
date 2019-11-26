1fc47c3147955e426f3a8ffe49c02e2ba86f5952
Ergodic Mage <ErgodicMage@gmail.com>
2019-11-25 21:52:01 -0600

    Updated CSV library and Added in caculating closest stars.

---
+++ tests/ProgressionTests/LocalSectorStarsReader.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+using CsvHelper;
+using CsvHelper.Configuration;
+
+using StellarMap.Math.Types;
+
+namespace ProgressionTests
+{
+    public class LocalSectorStar
+    {
+        public LocalSectorStar()
+        {
+        }
+
+        public string Designation { get; set; }
+        public Point3d Position { get; set; }
+        public string Name { get; set; }
+        public string Cluster { get; set; }
+    }
+
+    public class LocalSectorStarsReader
+    {
+        public IList<LocalSectorStar> Stars { get; set; }
+
+        public void Load(string starsFile)
+        {
+            Stars = new List<LocalSectorStar>();
+
+            CsvReader reader = new CsvReader(File.OpenText(starsFile));
+            
+            reader.Read();
+            reader.ReadHeader();
+            while (reader.Read())
+            {
+                string designation = reader["Designation"];
+
+                if (string.IsNullOrEmpty(designation) || designation.StartsWith("A("))
+                    continue;
+
+                LocalSectorStar star = new LocalSectorStar();
+                star.Designation = designation;
+
+                string pos = reader["Position"];
+                if (!string.IsNullOrEmpty(pos) && Point3d.TryParse(pos, out var position))
+                    star.Position = position;
+
+                star.Name = reader["Name"];
+                star.Cluster = reader["Cluster"];
+
+                Stars.Add(star);
+            }
+        }
+    }
+}
--- tests/ProgressionTests/MiscTests.cs
+++ tests/ProgressionTests/MiscTests.cs
@@ -1,8 +1,11 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 using StellarMap.Core.Types;
-
+using StellarMap.Math;
+using StellarMap.Math.Types;
 using StellarMap.Progression;
 using StellarMap.Progression.DefaultSettingMaps;
 
@@ -32,13 +35,44 @@ namespace ProgressionTests
             {
                 foreach (ERBridge bridge in bridges)
                 {                   
-                    if (double.TryParse(bridge.BasicProperties[Constants.PropertyNames.Distance], out var distance))
+                    if (double.TryParse(bridge.BasicProperties[StellarMap.Core.Types.Constants.PropertyNames.Distance], out var distance))
                     {
                         distance = distance * 3.261633;
                         sw.Write(bridge.Name);
                         sw.Write(":");
                         sw.WriteLine(distance.ToString());
                     }               
+                }
+            }
+        }
+
+        [TestMethod]
+        public void NearestStars()
+        {
+            LocalSectorStarsReader reader = new LocalSectorStarsReader();
+            reader.Load(folder + "LocalSector Stars.csv");
+            IList<LocalSectorStar> stars = reader.Stars;
+
+            string starName = "Sun";
+            LocalSectorStar star = stars.Where(s => s.Name == starName).First();
+
+            double distance = 10 / 3.261633;
+            var nearest = stars.Where(s => AstronomicalFunctions.Distance(s.Position, star.Position) <= distance);
+
+            if (nearest != null)
+            {
+                using (StreamWriter writer = new StreamWriter(folder + "Closest.txt"))
+                {
+                    foreach (LocalSectorStar s in nearest)
+                    {
+                        writer.Write(s.Designation);
+                        writer.Write(" - ");
+                        writer.Write(s.Name);
+                        writer.Write(" - ");
+                        writer.Write(s.Cluster);
+                        writer.Write(" - ");
+                        writer.WriteLine(AstronomicalFunctions.Distance(s.Position, star.Position) * 3.261633);
+                    }
                 }
             }
         }
